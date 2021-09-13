c5075ad09ce014032b8edb5b686b674c6f751c5b
pedro <bertucci.dev@gmail.com>
2021-09-13 12:21:43 +0100

    setup project

---
+++ app/src/main/java/com/jetpack/compose/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.jetpack.compose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = "Hey look some text")
+                Spacer(modifier = Modifier.padding(top = 8.dp))
+                Button(onClick = {  }) {
+                    Text(text = "Click here")
+                }
+            }
+        }
+    }
+}
